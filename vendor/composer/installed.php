ed0cb337108321a30a5506a7c558fe6849a12842
Calvin Alkan <calvin@snicco.io>
2022-10-08 12:46:51 +0200

    5.9.3

---
+++ vendor/composer/installed.php
@@ -0,0 +1,38 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'vendor_name/stubs-bug',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'giacocorsiglia/wordpress-stubs' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '*',
+            ),
+        ),
+        'php-stubs/wordpress-stubs' => array(
+            'pretty_version' => 'v5.9.3',
+            'version' => '5.9.3.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../php-stubs/wordpress-stubs',
+            'aliases' => array(),
+            'reference' => '18d56875e5078a50b8ea4bc4b20b735ca61edeee',
+            'dev_requirement' => false,
+        ),
+        'vendor_name/stubs-bug' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
